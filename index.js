c8bca720ebc0cc78070ed463290edcea8ec7359e
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-05-09 12:06:36 +0600

    dynamic data according to id

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ app.get('/chefData', (req, res) => {
     res.send(allChefData);
 });
 
+app.get('/chefData/:id', (req, res) => {
+    const chefId = req.params.id;
+    const chefData = allChefData.find(chef => chef.id === parseInt(chefId));
+    res.send(chefData);
+});
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
