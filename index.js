bd5bef25bbcbb27d5e0e45fcef4ff07f3bfdfa3e
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-05-09 12:05:21 +0600

    Get data in LH

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+
 const express = require('express');
 const app = express();
+
+const cors = require("cors");
+
 const port = process.env.PORT || 5555;
+
+app.use(cors());
+
+const allChefData = require("./chefData.json");
 
 app.get('/', (req, res) => {
     res.send('Savoury Bite Server');
 });
 
+app.get('/chefData', (req, res) => {
+    res.send(allChefData);
+});
+
 app.listen(port, () => {
-    console.log(`Savoury Bite Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
