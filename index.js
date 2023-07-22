36577df9f83efd2202b79f39102c3adf085ced79
Mujahidul12628 <mujahidul15-12628@diu.edu.bd>
2023-07-23 01:34:07 +0600

    dynamic data added

--- index.js
+++ index.js
@@ -6,24 +6,37 @@ const cors = require("cors");
 
 const port = process.env.PORT || 5555;
 
+const chef = require("./jsonData/chef.json");
+const recipes = require("./jsonData/recipes.json");
+
 app.use(cors());
 
-const allChefData = require("./chefData.json");
-
-app.get('/', (req, res) => {
-    res.send('Savoury Bite Server');
+// send all chef
+app.get("/chef", (req, res) => {
+    res.send(chef);
 });
 
-app.get('/chefData', (req, res) => {
-    res.send(allChefData);
+// send all recipes
+app.get("/recipes", (req, res) => {
+    res.send(recipes);
 });
 
-app.get('/chefData/:id', (req, res) => {
+// send specific recipe
+app.get("/recipe/:id", (req, res) => {
+    const id = req.params.id;
+    const selectedRecipe = recipes.find((recipe) => recipe.recipe_id === id);
+    res.send(selectedRecipe);
+});
+
+// send specific chef recipes
+app.get("/chef/:id", (req, res) => {
     const chefId = req.params.id;
-    const chefData = allChefData.find(chef => chef.id === parseInt(chefId));
-    res.send(chefData);
+    const selectedList = recipes.filter((recipe) => recipe.chef_id === chefId);
+    res.send(selectedList);
 });
+
+
 
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+    console.log(`server is running on port: ${port}`);
 });
